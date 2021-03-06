7fd3d9f44e6c8561d1bbbf6474d2698b10abc7fd
philomagi <tooppoo56@gmail.com>
2021-03-06 12:49:11 +0900

    RED: もういっこMの文字列表現テスト追加

--- strawbery_test.go
+++ strawbery_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_あまおうL文字列表現の取得する(t *testing.T) {
+func Test_あまおうLの文字列表現を取得する(t *testing.T) {
 	sut := Strawberry{
 		kind: "あまおう",
 		size: "L",
@@ -13,4 +13,13 @@ func Test_あまおうL文字列表現の取得する(t *testing.T) {
 	actual := sut.ToString()
 
 	assert.Equal(t, "あまおう: L", actual)
+}
+func Test_もういっこMの文字列表現を取得する(t *testing.T) {
+	sut := Strawberry{
+		kind: "もういっこ",
+		size: "M",
+	}
+	actual := sut.ToString()
+
+	assert.Equal(t, "もういっこ: M", actual)
 }
